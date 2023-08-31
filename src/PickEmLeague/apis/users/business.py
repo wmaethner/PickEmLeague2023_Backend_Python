6b34b50bf910dfd3b432d5f53fc02459bf82cb27
Will Maethner <wmaethner@developertown.com>
2023-08-31 19:20:23 -0400

    Update business.py

--- src/PickEmLeague/apis/users/business.py
+++ src/PickEmLeague/apis/users/business.py
@@ -1,9 +1,3 @@
-from http import HTTPStatus
-
-from flask import jsonify
-from flask_restx import abort
-
-from src.PickEmLeague import db
 from src.PickEmLeague.models.user import User
 from src.PickEmLeague.schemas.core.base_schema import BaseModel
 
@@ -16,24 +10,9 @@ def get_current_user(request):
     user = User.find_by_id(result.value["id"])
     if not user:
         return BaseModel.ErrorResult(f"no user found with id {result.value['id']}")
-    return BaseModel.SuccessResult(user.to_json())
-
-
-def create_user(user_dict):
-    email = user_dict["email"]
-    if User.find_by_email(email):
-        error = f"User email: {email} already exists, must be unique."
-        abort(HTTPStatus.CONFLICT, error, status="fail")
-    user = User(**user_dict)
-    db.session.add(user)
-    db.session.commit()
-    response = jsonify(status="success", message=f"New user added: {email}.")
-    response.status_code = HTTPStatus.CREATED
-    # response.headers["Location"] = url_for("api.user", email=email)
-    return response
+    return BaseModel.SuccessResult(user)
 
 
 def get_user_list():
     users = User.find_all()
-    # return BaseModel.SuccessResult([user.to_json() for user in users])
     return BaseModel.SuccessResult(users)
