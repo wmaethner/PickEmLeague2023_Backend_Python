31381a6b745f08645d0e958be18ded95f52d3806
Will Maethner <wmaethner@developertown.com>
2023-08-31 20:45:34 -0400

    Update endpoints.py

--- src/PickEmLeague/apis/game_picks/endpoints.py
+++ src/PickEmLeague/apis/game_picks/endpoints.py
@@ -24,8 +24,7 @@ game_picks_ns.add_models([game_pick_schema, game_pick_model, game_pick_list_mode
 
 @game_picks_ns.route("")
 class GamePickList(Resource):
-    @game_picks_ns.response(HTTPStatus.OK, "Retrieved team list.", game_pick_model)
-    @game_picks_ns.marshal_list_with(game_pick_model)
+    @game_picks_ns.marshal_with(game_pick_list_model)
     def get(self):
         """Retrieve a list of game picks."""
         return get_game_pick_list()
@@ -34,7 +33,7 @@ class GamePickList(Resource):
 @game_picks_ns.route("/<int:week>")
 @game_picks_ns.param("week", "Week number")
 class GamePicksByWeek(Resource):
-    @game_picks_ns.marshal_list_with(game_pick_model)
+    @game_picks_ns.marshal_with(game_pick_list_model)
     def get(self, week):
         return get_game_picks_by_week(week)
 
@@ -43,6 +42,6 @@ class GamePicksByWeek(Resource):
 @game_picks_ns.param("week", "Week number")
 @game_picks_ns.param("user_id", "User ID")
 class GamePicksByUserAndWeek(Resource):
-    @game_picks_ns.marshal_list_with(game_pick_model)
+    @game_picks_ns.marshal_with(game_pick_list_model)
     def get(self, week, user_id):
         return get_game_picks_by_user_and_week(user_id, week)
