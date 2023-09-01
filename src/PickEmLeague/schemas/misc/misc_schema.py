ca0292645208b8cde3eea517cda1af21f44197b8
Will Maethner <wmaethner@developertown.com>
2023-08-31 20:00:57 -0400

    Update misc_schema.py

--- src/PickEmLeague/schemas/misc/misc_schema.py
+++ src/PickEmLeague/schemas/misc/misc_schema.py
@@ -1,8 +1,8 @@
 from flask_restx import Model
-from flask_restx.fields import String
+from flask_restx.fields import Boolean
 
 from ..core.base_schema import BaseModel
 
-misc_schema = Model("MiscSchema", {"token": String})
+misc_schema = Model("MiscSchema", {"started": Boolean})
 
 misc_model = BaseModel("MiscModel", misc_schema).model()
