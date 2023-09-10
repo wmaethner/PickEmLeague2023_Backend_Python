8932e1e83435d8e44e69d236d9c643cbb891b25d
Will Maethner <wmaethner@developertown.com>
2023-09-10 14:48:02 -0400

    update user password

--- src/PickEmLeague/apis/users/business.py
+++ src/PickEmLeague/apis/users/business.py
@@ -1,5 +1,7 @@
+from src.PickEmLeague import db
 from src.PickEmLeague.models.user import User
 from src.PickEmLeague.schemas.core.base_schema import BaseModel
+from src.PickEmLeague.util.models import update_model
 
 
 def get_current_user(request):
@@ -16,3 +18,22 @@ def get_current_user(request):
 def get_user_list():
     users = User.find_all()
     return BaseModel.SuccessResult(users)
+
+
+def get_user(id: int):
+    user = User.find_by_id(id)
+    return BaseModel.SuccessResult(user)
+
+
+def update_user(id: int, user_data: any):
+    user = User.find_by_id(id)
+    if user:
+        update_model(user, user_data, ["password"], db)
+        return BaseModel.SuccessResult(User.find_by_id(id))
+    return BaseModel.ErrorResult(f"User with id {id} not found")
+
+
+def update_user_password(id: int, password: str):
+    user = User.find_by_id(id)
+    user.password = password
+    db.session.commit()
--- src/PickEmLeague/apis/users/endpoints.py
+++ src/PickEmLeague/apis/users/endpoints.py
@@ -1,5 +1,5 @@
 from flask import jsonify, request
-from flask_restx import Resource
+from flask_restx import Resource, fields
 
 from src.PickEmLeague.decorators.auth import token_required
 from src.PickEmLeague.models.user import User
@@ -7,7 +7,13 @@ from src.PickEmLeague.schemas.users.user_list_schema import user_list_model
 from src.PickEmLeague.schemas.users.user_schema import user_model, user_schema
 
 from ..core.base_namespace import BaseNamespace
-from .business import get_current_user, get_user_list
+from .business import (
+    get_current_user,
+    get_user,
+    get_user_list,
+    update_user,
+    update_user_password,
+)
 
 user_ns = BaseNamespace(name="users", validate=True)
 user_ns.add_models([user_schema, user_model, user_list_model])
@@ -35,14 +41,25 @@ class UserList(Resource):
 class UserById(Resource):
     @user_ns.marshal_with(user_model)
     def get(self, id):
-        """Retrieve a list of users."""
-        user_ns.logger.info("Getting user list")
-        try:
-            user = jsonify(User.find_by_id(id))
-            print(user.data)
-        except Exception as e:
-            print(e)
-        return get_user_list()
+        return get_user(id)
+
+    @user_ns.expect(user_schema)
+    def put(self, id):
+        return update_user(id, request.get_json())
+
+
+@user_ns.route("/password/<int:id>")
+class UpdateUserPassword(Resource):
+    @user_ns.expect(
+        user_ns.model(
+            "UserPassword",
+            {
+                "password": fields.String,
+            },
+        )
+    )
+    def put(self, id):
+        return update_user_password(id, request.get_json()["password"])
 
 
 @user_ns.route("/clear-all")
