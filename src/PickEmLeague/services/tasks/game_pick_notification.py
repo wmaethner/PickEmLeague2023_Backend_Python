89c2f4fb9e1f1ef8e7049728fcd39b6cd00356a8
Will Maethner <wmaethner@developertown.com>
2023-10-20 11:40:51 -0400

    Update game_pick_notification.py

--- src/PickEmLeague/services/tasks/game_pick_notification.py
+++ src/PickEmLeague/services/tasks/game_pick_notification.py
@@ -1,3 +1,4 @@
+from src.PickEmLeague import scheduler
 from src.PickEmLeague.models.game import Game
 from src.PickEmLeague.models.user import User
 from src.PickEmLeague.services.push_notifications.send_notification import (
@@ -6,8 +7,9 @@ from src.PickEmLeague.services.push_notifications.send_notification import (
 
 
 def game_pick_notification(user_id: int, game_id: int):
-    user = User.find_by_id(user_id)
-    game = Game.find_by_id(game_id)
+    with scheduler.app.app_context():
+        user = User.find_by_id(user_id)
+        game = Game.find_by_id(game_id)
 
-    message = f"Make pick for {game.away_team.name} @ {game.home_team.name}"
-    send_notification(user, message)
+        message = f"Make pick for {game.away_team.name} @ {game.home_team.name}"
+        send_notification(user, message)
