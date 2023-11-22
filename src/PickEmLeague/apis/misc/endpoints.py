add3ccf1c72ce1328821fe331a6bcaee3c4bda24
Will Maethner <wmaethner@developertown.com>
2023-11-21 21:00:48 -0500

    Update endpoints.py

--- src/PickEmLeague/apis/misc/endpoints.py
+++ src/PickEmLeague/apis/misc/endpoints.py
@@ -30,7 +30,7 @@ class MiscInfo(Resource):
         return BaseModel.SuccessResult(
             {
                 "started": True,
-                "current_week": 11,
+                "current_week": 12,
                 "versions": {"ios": 13, "android": 9, "server": server_version},
             }
         )
